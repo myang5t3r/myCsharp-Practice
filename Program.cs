c6c41f3f5058b27ff48ac7d141d1e41e184392dc
myang5t3r <matthew.yang@hunterdouglas.com>
2022-12-13 13:14:38 -0700

    C# Practice

--- Program.cs
+++ Program.cs
@@ -6,6 +6,22 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
+        // string greeting = "Hello";
+        // greeting = greeting + "World";
+        // Console.WriteLine("greeting" + greeting);
+
+        // Console.WriteLine($"greeting: {greeting}"); 
+        // Console.WriteLine("greeting: {0}", greeting);
+
+        // How do you find the area of a square? Area = side * side
+        // float side = 3.14;
+
+        // float side = 3.14F;
+        // float area = side * side;
+        // Console.WriteLine("area: {0}", area);
+
+        // Console.WriteLine("area is a {0}", area.GetType());
+
         // Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
         // Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
         // Console.WriteLine(1 + 2 * 3);     // order of operations
