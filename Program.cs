27596a3a814b1fc0ba2fe02d9a58000328ddc249
myang5t3r <matthew.yang@hunterdouglas.com>
2022-12-13 13:02:44 -0700

    inital commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
+        Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
+        Console.WriteLine(1 + 2 * 3);     // order of operations
+        Console.WriteLine(10 / 3.0);      // int's and doubles
+        Console.WriteLine(10 / 3);        // int's 
+        Console.WriteLine("12" + "3");    // What happens here?
+
+        int num = 10;
+        num += 100;
+        Console.WriteLine(num);
+        num ++;
+        Console.WriteLine(num);
+    }
+  }
+}
