8110e7beb346b34c96b663c0c9e9dbd4057d4eb8
myang5t3r <matthew.yang@hunterdouglas.com>
2022-12-13 13:12:32 -0700

    Boolean valuse

--- Program.cs
+++ Program.cs
@@ -6,18 +6,22 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-        Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
-        Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
-        Console.WriteLine(1 + 2 * 3);     // order of operations
-        Console.WriteLine(10 / 3.0);      // int's and doubles
-        Console.WriteLine(10 / 3);        // int's 
-        Console.WriteLine("12" + "3");    // What happens here?
+        // Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
+        // Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
+        // Console.WriteLine(1 + 2 * 3);     // order of operations
+        // Console.WriteLine(10 / 3.0);      // int's and doubles
+        // Console.WriteLine(10 / 3);        // int's 
+        // Console.WriteLine("12" + "3");    // What happens here?
 
-        int num = 10;
-        num += 100;
-        Console.WriteLine(num);
-        num ++;
-        Console.WriteLine(num);
+        // int num = 10;
+        // num += 100;
+        // Console.WriteLine(num);
+        // num ++;
+        // Console.WriteLine(num);
+
+        bool isCold = true;
+        Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
+        Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
     }
   }
 }
