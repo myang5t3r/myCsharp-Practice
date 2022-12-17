84462aa0bcf12c9c492f0157329b8166f7b7ee51
myang5t3r <matthew.yang@hunterdouglas.com>
2022-12-17 08:55:29 -0700

    Create Employee class, instanciate in Program.cs, Employee class is used as a custom type

--- Program.cs
+++ Program.cs
@@ -3,46 +3,50 @@ using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-  class Program
-  {
-      static List<string> GetEmployees()
-      {
+    class Program
+    {
+        static List<Employee> GetEmployees()
+        {
             // return a list of strings
             // Create list of employees
-            List<string> employees =  new List<string>();
+            List<Employee> employees = new List<Employee>();
             while (true)
             {
-            Console.WriteLine("Please enter a name:");
-            // use ReadLine method to assign user input to a variable
-            // note: ReadLine causes runtime halt and waits for input
+                Console.WriteLine("Please enter a name:");
+                // use ReadLine method to assign user input to a variable
+                // note: ReadLine causes runtime halt and waits for input
 
-            // note coalescing operator ?? - similar to ternary operator in js
-            // operator checks for null value and if null replace with "" (empty string)
-            string input = Console.ReadLine() ?? "";
-            if (input == "")
-            {
-                  break;
-            }
-            employees.Add(input);
+                // note coalescing operator ?? - similar to ternary operator in js
+                // operator checks for null value and if null replace with "" (empty string)
+                string input = Console.ReadLine() ?? "";
+                if (input == "")
+                {
+                    break;
+                }
+                // Create new employee instance
+                Employee currentEmployee = new Employee(input, "Yang");
+                //Add currentEmployee not a string
+                employees.Add(currentEmployee);
             }
             return employees;
-      }
+        }
 
-      static void PrintEmployees(List<string> employees)
-      {
+        static void PrintEmployees(List<Employee> employees)
+        {
             // print employees list to console
-            foreach (string employee in employees )
+            for (int i = 0; i < employees.Count; i++)
             {
-                  Console.WriteLine(employee);
+                Console.WriteLine(employees[i].GetFullName());
             }
-      }
-      static void Main(string[] args)
-      {
+        }
+        static void Main(string[] args)
+        {
             // Call method to get employees
-            List<string> employees = GetEmployees();
-            
+            List<Employee> employees = GetEmployees();
+
             // Call method to print employees
             PrintEmployees(employees);
-      }
-  }
+        }
+    }
+
 }
