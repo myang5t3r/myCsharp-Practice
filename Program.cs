5b64bda131595c13296e41d081648bf1b0c83757
myang5t3r <matthew.yang@hunterdouglas.com>
2022-12-18 09:25:28 -0700

    Add all employee properties to class and GetEmployee(), parse id to string

--- Program.cs
+++ Program.cs
@@ -18,13 +18,26 @@ namespace CatWorx.BadgeMaker
 
                 // note coalescing operator ?? - similar to ternary operator in js
                 // operator checks for null value and if null replace with "" (empty string)
-                string input = Console.ReadLine() ?? "";
-                if (input == "")
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
                 {
                     break;
                 }
+                // assign user input into variable to be passed to Employee class to store user data
+                Console.WriteLine("Please enter last name:");
+                string lastName = Console.ReadLine() ?? "";
+                if (lastName == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter user id");
+                // must cast into int32, ReadLine() returns only strings
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.WriteLine("Please enter photo url:");
+                string photoUrl = Console.ReadLine() ?? "";
+
                 // Create new employee instance
-                Employee currentEmployee = new Employee(input, "Yang");
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 //Add currentEmployee not a string
                 employees.Add(currentEmployee);
             }
@@ -50,3 +63,6 @@ namespace CatWorx.BadgeMaker
     }
 
 }
+
+
+// NOTE @ #8
